a7a9180bef6ab1c66c16fbfce95d5cdea2b21628
gtroncone <gtroncone@PC-TRONCONE>
2016-11-19 15:15:13 +0100

    19/11/2016

--- Cambio Bolle/Form1.Designer.cs
+++ Cambio Bolle/Form1.Designer.cs
@@ -29,11 +29,353 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.cBxGross1 = new System.Windows.Forms.CheckBox();
+            this.cBxgross2 = new System.Windows.Forms.CheckBox();
+            this.cBxgross3 = new System.Windows.Forms.CheckBox();
+            this.cBxgross4 = new System.Windows.Forms.CheckBox();
+            this.cBxgross5 = new System.Windows.Forms.CheckBox();
+            this.cBxgross6 = new System.Windows.Forms.CheckBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.tbxCodGrossDaSost1 = new System.Windows.Forms.TextBox();
+            this.tbxCodGrosscheSost1 = new System.Windows.Forms.TextBox();
+            this.tbxCodGrossDaSost2 = new System.Windows.Forms.TextBox();
+            this.tbxCodGrosscheSost2 = new System.Windows.Forms.TextBox();
+            this.tbxCodGrossDaSost3 = new System.Windows.Forms.TextBox();
+            this.tbxCodGrosscheSost3 = new System.Windows.Forms.TextBox();
+            this.tbxCodGrossDaSost4 = new System.Windows.Forms.TextBox();
+            this.tbxCodGrosscheSost4 = new System.Windows.Forms.TextBox();
+            this.tbxCodGrossDaSost5 = new System.Windows.Forms.TextBox();
+            this.tbxCodGrosscheSost5 = new System.Windows.Forms.TextBox();
+            this.tbxCodGrossDaSost6 = new System.Windows.Forms.TextBox();
+            this.tbxCodGrosscheSost6 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.cBxTimerAut = new System.Windows.Forms.CheckBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox4.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // cBxGross1
+            // 
+            this.cBxGross1.AutoSize = true;
+            this.cBxGross1.Location = new System.Drawing.Point(24, 56);
+            this.cBxGross1.Name = "cBxGross1";
+            this.cBxGross1.Size = new System.Drawing.Size(78, 17);
+            this.cBxGross1.TabIndex = 0;
+            this.cBxGross1.Text = "Grossista 1";
+            this.cBxGross1.UseVisualStyleBackColor = true;
+            // 
+            // cBxgross2
+            // 
+            this.cBxgross2.AutoSize = true;
+            this.cBxgross2.Location = new System.Drawing.Point(24, 116);
+            this.cBxgross2.Name = "cBxgross2";
+            this.cBxgross2.Size = new System.Drawing.Size(78, 17);
+            this.cBxgross2.TabIndex = 1;
+            this.cBxgross2.Text = "Grossista 2";
+            this.cBxgross2.UseVisualStyleBackColor = true;
+            // 
+            // cBxgross3
+            // 
+            this.cBxgross3.AutoSize = true;
+            this.cBxgross3.Location = new System.Drawing.Point(24, 170);
+            this.cBxgross3.Name = "cBxgross3";
+            this.cBxgross3.Size = new System.Drawing.Size(78, 17);
+            this.cBxgross3.TabIndex = 2;
+            this.cBxgross3.Text = "Grossista 3";
+            this.cBxgross3.UseVisualStyleBackColor = true;
+            // 
+            // cBxgross4
+            // 
+            this.cBxgross4.AutoSize = true;
+            this.cBxgross4.Location = new System.Drawing.Point(24, 228);
+            this.cBxgross4.Name = "cBxgross4";
+            this.cBxgross4.Size = new System.Drawing.Size(78, 17);
+            this.cBxgross4.TabIndex = 3;
+            this.cBxgross4.Text = "Grossista 4";
+            this.cBxgross4.UseVisualStyleBackColor = true;
+            // 
+            // cBxgross5
+            // 
+            this.cBxgross5.AutoSize = true;
+            this.cBxgross5.Location = new System.Drawing.Point(24, 285);
+            this.cBxgross5.Name = "cBxgross5";
+            this.cBxgross5.Size = new System.Drawing.Size(78, 17);
+            this.cBxgross5.TabIndex = 4;
+            this.cBxgross5.Text = "Grossista 5";
+            this.cBxgross5.UseVisualStyleBackColor = true;
+            // 
+            // cBxgross6
+            // 
+            this.cBxgross6.AutoSize = true;
+            this.cBxgross6.Location = new System.Drawing.Point(24, 345);
+            this.cBxgross6.Name = "cBxgross6";
+            this.cBxgross6.Size = new System.Drawing.Size(78, 17);
+            this.cBxgross6.TabIndex = 5;
+            this.cBxgross6.Text = "Grossista 6";
+            this.cBxgross6.UseVisualStyleBackColor = true;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 30000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // tbxCodGrossDaSost1
+            // 
+            this.tbxCodGrossDaSost1.Location = new System.Drawing.Point(29, 53);
+            this.tbxCodGrossDaSost1.Name = "tbxCodGrossDaSost1";
+            this.tbxCodGrossDaSost1.Size = new System.Drawing.Size(100, 20);
+            this.tbxCodGrossDaSost1.TabIndex = 1;
+            // 
+            // tbxCodGrosscheSost1
+            // 
+            this.tbxCodGrosscheSost1.Location = new System.Drawing.Point(33, 53);
+            this.tbxCodGrosscheSost1.Name = "tbxCodGrosscheSost1";
+            this.tbxCodGrosscheSost1.Size = new System.Drawing.Size(100, 20);
+            this.tbxCodGrosscheSost1.TabIndex = 2;
+            // 
+            // tbxCodGrossDaSost2
+            // 
+            this.tbxCodGrossDaSost2.Location = new System.Drawing.Point(29, 113);
+            this.tbxCodGrossDaSost2.Name = "tbxCodGrossDaSost2";
+            this.tbxCodGrossDaSost2.Size = new System.Drawing.Size(100, 20);
+            this.tbxCodGrossDaSost2.TabIndex = 3;
+            // 
+            // tbxCodGrosscheSost2
+            // 
+            this.tbxCodGrosscheSost2.Location = new System.Drawing.Point(33, 113);
+            this.tbxCodGrosscheSost2.Name = "tbxCodGrosscheSost2";
+            this.tbxCodGrosscheSost2.Size = new System.Drawing.Size(100, 20);
+            this.tbxCodGrosscheSost2.TabIndex = 4;
+            // 
+            // tbxCodGrossDaSost3
+            // 
+            this.tbxCodGrossDaSost3.Location = new System.Drawing.Point(29, 167);
+            this.tbxCodGrossDaSost3.Name = "tbxCodGrossDaSost3";
+            this.tbxCodGrossDaSost3.Size = new System.Drawing.Size(100, 20);
+            this.tbxCodGrossDaSost3.TabIndex = 5;
+            // 
+            // tbxCodGrosscheSost3
+            // 
+            this.tbxCodGrosscheSost3.Location = new System.Drawing.Point(33, 167);
+            this.tbxCodGrosscheSost3.Name = "tbxCodGrosscheSost3";
+            this.tbxCodGrosscheSost3.Size = new System.Drawing.Size(100, 20);
+            this.tbxCodGrosscheSost3.TabIndex = 6;
+            // 
+            // tbxCodGrossDaSost4
+            // 
+            this.tbxCodGrossDaSost4.Location = new System.Drawing.Point(29, 225);
+            this.tbxCodGrossDaSost4.Name = "tbxCodGrossDaSost4";
+            this.tbxCodGrossDaSost4.Size = new System.Drawing.Size(100, 20);
+            this.tbxCodGrossDaSost4.TabIndex = 7;
+            // 
+            // tbxCodGrosscheSost4
+            // 
+            this.tbxCodGrosscheSost4.Location = new System.Drawing.Point(33, 225);
+            this.tbxCodGrosscheSost4.Name = "tbxCodGrosscheSost4";
+            this.tbxCodGrosscheSost4.Size = new System.Drawing.Size(100, 20);
+            this.tbxCodGrosscheSost4.TabIndex = 8;
+            // 
+            // tbxCodGrossDaSost5
+            // 
+            this.tbxCodGrossDaSost5.Location = new System.Drawing.Point(29, 282);
+            this.tbxCodGrossDaSost5.Name = "tbxCodGrossDaSost5";
+            this.tbxCodGrossDaSost5.Size = new System.Drawing.Size(100, 20);
+            this.tbxCodGrossDaSost5.TabIndex = 9;
+            // 
+            // tbxCodGrosscheSost5
+            // 
+            this.tbxCodGrosscheSost5.Location = new System.Drawing.Point(33, 282);
+            this.tbxCodGrosscheSost5.Name = "tbxCodGrosscheSost5";
+            this.tbxCodGrosscheSost5.Size = new System.Drawing.Size(100, 20);
+            this.tbxCodGrosscheSost5.TabIndex = 10;
+            // 
+            // tbxCodGrossDaSost6
+            // 
+            this.tbxCodGrossDaSost6.Location = new System.Drawing.Point(29, 345);
+            this.tbxCodGrossDaSost6.Name = "tbxCodGrossDaSost6";
+            this.tbxCodGrossDaSost6.Size = new System.Drawing.Size(100, 20);
+            this.tbxCodGrossDaSost6.TabIndex = 11;
+            // 
+            // tbxCodGrosscheSost6
+            // 
+            this.tbxCodGrosscheSost6.Location = new System.Drawing.Point(33, 345);
+            this.tbxCodGrosscheSost6.Name = "tbxCodGrosscheSost6";
+            this.tbxCodGrosscheSost6.Size = new System.Drawing.Size(100, 20);
+            this.tbxCodGrosscheSost6.TabIndex = 12;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(21, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(133, 13);
+            this.label1.TabIndex = 18;
+            this.label1.Text = "Codice Gross Da Sostituire";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 18);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(146, 13);
+            this.label2.TabIndex = 19;
+            this.label2.Text = "Codice Gross Che Sostituisce";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.tbxCodGrosscheSost1);
+            this.groupBox1.Controls.Add(this.tbxCodGrosscheSost2);
+            this.groupBox1.Controls.Add(this.tbxCodGrosscheSost6);
+            this.groupBox1.Controls.Add(this.tbxCodGrosscheSost3);
+            this.groupBox1.Controls.Add(this.tbxCodGrosscheSost4);
+            this.groupBox1.Controls.Add(this.tbxCodGrosscheSost5);
+            this.groupBox1.Location = new System.Drawing.Point(331, 13);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(162, 406);
+            this.groupBox1.TabIndex = 20;
+            this.groupBox1.TabStop = false;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Controls.Add(this.tbxCodGrossDaSost1);
+            this.groupBox2.Controls.Add(this.tbxCodGrossDaSost6);
+            this.groupBox2.Controls.Add(this.tbxCodGrossDaSost2);
+            this.groupBox2.Controls.Add(this.tbxCodGrossDaSost3);
+            this.groupBox2.Controls.Add(this.tbxCodGrossDaSost4);
+            this.groupBox2.Controls.Add(this.tbxCodGrossDaSost5);
+            this.groupBox2.Location = new System.Drawing.Point(165, 13);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(160, 406);
+            this.groupBox2.TabIndex = 21;
+            this.groupBox2.TabStop = false;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.groupBox3.Controls.Add(this.cBxgross6);
+            this.groupBox3.Controls.Add(this.cBxGross1);
+            this.groupBox3.Controls.Add(this.cBxgross2);
+            this.groupBox3.Controls.Add(this.cBxgross3);
+            this.groupBox3.Controls.Add(this.cBxgross4);
+            this.groupBox3.Controls.Add(this.cBxgross5);
+            this.groupBox3.Location = new System.Drawing.Point(13, 13);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(146, 406);
+            this.groupBox3.TabIndex = 22;
+            this.groupBox3.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button1.Location = new System.Drawing.Point(13, 423);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(146, 61);
+            this.button1.TabIndex = 23;
+            this.button1.Text = "SALVA ed ESCI";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // cBxTimerAut
+            // 
+            this.cBxTimerAut.AutoSize = true;
+            this.cBxTimerAut.Location = new System.Drawing.Point(12, 20);
+            this.cBxTimerAut.Name = "cBxTimerAut";
+            this.cBxTimerAut.Size = new System.Drawing.Size(132, 17);
+            this.cBxTimerAut.TabIndex = 24;
+            this.cBxTimerAut.Text = "attiva timer automatico";
+            this.cBxTimerAut.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button2.Location = new System.Drawing.Point(165, 423);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(160, 61);
+            this.button2.TabIndex = 25;
+            this.button2.Text = "SOSTITUISCI";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.groupBox4.Controls.Add(this.cBxTimerAut);
+            this.groupBox4.Location = new System.Drawing.Point(331, 426);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(161, 60);
+            this.groupBox4.TabIndex = 26;
+            this.groupBox4.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(504, 498);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox4);
+            this.Name = "Form1";
+            this.Text = "Cambio Bolle";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.groupBox4.ResumeLayout(false);
+            this.groupBox4.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.CheckBox cBxGross1;
+        private System.Windows.Forms.CheckBox cBxgross2;
+        private System.Windows.Forms.CheckBox cBxgross3;
+        private System.Windows.Forms.CheckBox cBxgross4;
+        private System.Windows.Forms.CheckBox cBxgross5;
+        private System.Windows.Forms.CheckBox cBxgross6;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TextBox tbxCodGrossDaSost1;
+        private System.Windows.Forms.TextBox tbxCodGrosscheSost1;
+        private System.Windows.Forms.TextBox tbxCodGrossDaSost2;
+        private System.Windows.Forms.TextBox tbxCodGrosscheSost2;
+        private System.Windows.Forms.TextBox tbxCodGrossDaSost3;
+        private System.Windows.Forms.TextBox tbxCodGrosscheSost3;
+        private System.Windows.Forms.TextBox tbxCodGrossDaSost4;
+        private System.Windows.Forms.TextBox tbxCodGrosscheSost4;
+        private System.Windows.Forms.TextBox tbxCodGrossDaSost5;
+        private System.Windows.Forms.TextBox tbxCodGrosscheSost5;
+        private System.Windows.Forms.TextBox tbxCodGrossDaSost6;
+        private System.Windows.Forms.TextBox tbxCodGrosscheSost6;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.CheckBox cBxTimerAut;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.GroupBox groupBox4;
     }
 }
 
--- Cambio Bolle/Form1.cs
+++ Cambio Bolle/Form1.cs
@@ -7,14 +7,127 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using FirebirdSql.Data.FirebirdClient;
 
 namespace Cambio_Bolle
 {
     public partial class Form1 : Form
     {
-        public Form1()
+         FbConnection connessione = new FbConnection("User=SYSDBA;Password=masterkey;Database=c:/program files (x86)/winfarm/archivi/arc2000.phs;DataSource=localhost;");
+            public Form1()
+            {
+              InitializeComponent();
+            //DateTime today = DateTime.Now;
+            //TimeSpan tempo = new TimeSpan(-4, 0, 0, 0);
+            //;
+            //string data= (today.Add(tempo)).ToString("MM/dd/yyyy");
+            //textBox1.Text = data;
+            }
+        
+
+        private void Form1_Load(object sender, EventArgs e)
         {
-            InitializeComponent();
+            tbxCodGrossDaSost1.Text = Cambio_Bolle.Properties.Settings.Default.codgrosSost1;
+            tbxCodGrossDaSost2.Text = Cambio_Bolle.Properties.Settings.Default.codgrosSost2;
+            tbxCodGrossDaSost3.Text = Cambio_Bolle.Properties.Settings.Default.codgrosSost3;
+            tbxCodGrossDaSost4.Text = Cambio_Bolle.Properties.Settings.Default.codgrosSost4;
+            tbxCodGrossDaSost5.Text = Cambio_Bolle.Properties.Settings.Default.codgrosSost5;
+            tbxCodGrossDaSost6.Text = Cambio_Bolle.Properties.Settings.Default.codgrosSost6;
+            tbxCodGrosscheSost1.Text = Cambio_Bolle.Properties.Settings.Default.codgrosNuovo1;
+            tbxCodGrosscheSost2.Text = Cambio_Bolle.Properties.Settings.Default.codgrosNuovo2;
+            tbxCodGrosscheSost3.Text = Cambio_Bolle.Properties.Settings.Default.codgrosNuovo3;
+            tbxCodGrosscheSost4.Text = Cambio_Bolle.Properties.Settings.Default.codgrosNuovo4;
+            tbxCodGrosscheSost5.Text = Cambio_Bolle.Properties.Settings.Default.codgrosNuovo5;
+            tbxCodGrosscheSost6.Text = Cambio_Bolle.Properties.Settings.Default.codgrosNuovo6;
+            cBxGross1.Checked = Cambio_Bolle.Properties.Settings.Default.check1;
+            cBxgross2.Checked = Cambio_Bolle.Properties.Settings.Default.check2;
+            cBxgross3.Checked = Cambio_Bolle.Properties.Settings.Default.check3;
+            cBxgross4.Checked = Cambio_Bolle.Properties.Settings.Default.check4;
+            cBxgross5.Checked = Cambio_Bolle.Properties.Settings.Default.check5;
+            cBxgross6.Checked = Cambio_Bolle.Properties.Settings.Default.check6;
+            cBxTimerAut.Checked = Cambio_Bolle.Properties.Settings.Default.check7;
+            if (cBxTimerAut.Checked==true)
+            {
+                timer1.Enabled = true;
+               
+
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+            update();
+            this.Close();
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            Cambio_Bolle.Properties.Settings.Default.codgrosSost1 = tbxCodGrossDaSost1.Text;
+            Cambio_Bolle.Properties.Settings.Default.codgrosSost2=tbxCodGrossDaSost2.Text; 
+            Cambio_Bolle.Properties.Settings.Default.codgrosSost3=tbxCodGrossDaSost3.Text; 
+            Cambio_Bolle.Properties.Settings.Default.codgrosSost4=tbxCodGrossDaSost4.Text; 
+            Cambio_Bolle.Properties.Settings.Default.codgrosSost5=tbxCodGrossDaSost5.Text;
+            Cambio_Bolle.Properties.Settings.Default.codgrosSost6 = tbxCodGrossDaSost6.Text; 
+            Cambio_Bolle.Properties.Settings.Default.codgrosNuovo1 =tbxCodGrosscheSost1.Text ;
+            Cambio_Bolle.Properties.Settings.Default.codgrosNuovo2=tbxCodGrosscheSost2.Text;
+            Cambio_Bolle.Properties.Settings.Default.codgrosNuovo3=tbxCodGrosscheSost3.Text;
+            Cambio_Bolle.Properties.Settings.Default.codgrosNuovo4=tbxCodGrosscheSost4.Text;
+            Cambio_Bolle.Properties.Settings.Default.codgrosNuovo5=tbxCodGrosscheSost5.Text;
+            Cambio_Bolle.Properties.Settings.Default.codgrosNuovo6=tbxCodGrosscheSost6.Text;
+            
+            Cambio_Bolle.Properties.Settings.Default.check1 = cBxGross1.Checked;
+            Cambio_Bolle.Properties.Settings.Default.check2 = cBxgross2.Checked;
+            Cambio_Bolle.Properties.Settings.Default.check3 = cBxgross3.Checked;
+            Cambio_Bolle.Properties.Settings.Default.check4 = cBxgross4.Checked;
+            Cambio_Bolle.Properties.Settings.Default.check5 = cBxgross5.Checked;
+            Cambio_Bolle.Properties.Settings.Default.check6 = cBxgross6.Checked;
+            Cambio_Bolle.Properties.Settings.Default.check7 = cBxTimerAut.Checked;
+            Cambio_Bolle.Properties.Settings.Default.Save();
+            this.Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            update();
+            MessageBox.Show("UPDATE ESEGUITO CORRETTAMENTE");
+
+        }
+        public void update()
+        {
+            connessione.Open();
+            if (cBxGross1.Checked == true)
+            {
+                FbCommand update1 = new FbCommand("update ord_testa set codice='" + tbxCodGrosscheSost1.Text + "' where (tipo='SD' or tipo='SG') and codice='" + tbxCodGrossDaSost1.Text + "'", connessione);
+                update1.ExecuteNonQuery();
+            }
+            if (cBxgross2.Checked == true)
+            {
+                FbCommand update2 = new FbCommand("update ord_testa set codice='" + tbxCodGrosscheSost2.Text + "' where (tipo='SD' or tipo='SG') and codice='" + tbxCodGrossDaSost2 + "'", connessione);
+                update2.ExecuteNonQuery();
+            }
+            if (cBxgross3.Checked == true)
+            {
+                FbCommand update3 = new FbCommand("update ord_testa set codice='" + tbxCodGrosscheSost3.Text + "' where (tipo='SD' or tipo='SG') and codice='" + tbxCodGrossDaSost3 + "'", connessione);
+                update3.ExecuteNonQuery();
+            }
+            if (cBxgross4.Checked == true)
+            {
+                FbCommand update4 = new FbCommand("update ord_testa set codice='" + tbxCodGrosscheSost4.Text + "' where (tipo='SD' or tipo='SG') and codice='" + tbxCodGrossDaSost4 + "'", connessione);
+                update4.ExecuteNonQuery();
+            }
+            if (cBxgross5.Checked == true)
+            {
+                FbCommand update5 = new FbCommand("update ord_testa set codice='" + tbxCodGrosscheSost5.Text + "' where (tipo='SD' or tipo='SG') and codice='" + tbxCodGrossDaSost5 + "'", connessione);
+                update5.ExecuteNonQuery();
+            }
+            if (cBxgross6.Checked == true)
+            {
+                FbCommand update6 = new FbCommand("update ord_testa set codice='" + tbxCodGrosscheSost6.Text + "' where (tipo='SD' or tipo='SG') and codice='" + tbxCodGrossDaSost6 + "'", connessione);
+                update6.ExecuteNonQuery();
+            }
+            connessione.Close();
         }
     }
-}
+    }
+
